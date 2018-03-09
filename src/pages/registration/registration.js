7ba7d503c25b18d7af0ba0cd2181539e781cab3b
Peter Peck <karatekid4christ@gmail.com>
2018-03-08 21:40:15 -0600

    Staging changes

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -84,7 +84,7 @@ class SignupForm extends Component {
 			errArray = [];
 		} else {
 			this.setState({
-			username: '', password: '', confirmPassword: '', profilePicture: '', redirectTo: "/lobby", loggedin: true
+				username: '', password: '', confirmPassword: '', profilePicture: '', redirectTo: "/lobby", loggedin: true
 			});
 		}
 		}).catch((error) => {
