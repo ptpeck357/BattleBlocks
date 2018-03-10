b5244f1b7a8bef0e59410a50a932a351289eeb18
Peter Peck <karatekid4christ@gmail.com>
2018-03-09 19:49:19 -0600

    redirecting to lobby after endgame

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -208,7 +208,7 @@ class Gameboard extends React.Component {
   //Remove firebase game data
   componentWillUnmount() {
     this.endFirebase();
-    window.location.assign("https://battle-blocks.herokuapp.com");
+    window.location.assign("https://battle-blocks.herokuapp.com/lobby");
   };
 
 // ----------------------- ------------- -----------------------//
